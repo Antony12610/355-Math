bdecc9f3038224ffac1c6ae63e19587b9ad666b5
Antony Fuentes <antony@antonys-mbp.mynetworksettings.com>
2023-10-31 14:15:37 -0400

    my math program in java

--- math.java
+++ math.java
@@ -2,7 +2,6 @@ public class math {
     public int add(int a, int b) {
         return a + b;
     }
-
     public static void main(String[] args) {
         math math = new math();
         int result = math.add(5, 7);
